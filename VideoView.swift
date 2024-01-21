2f51b2fbb79f3798658f5de9d3b442ff1a35a15b
Karumaidoi <mainahmwangi12@gmail.com>
2024-01-21 04:52:57 +0300

    added support for url

--- VideoView.swift
+++ VideoView.swift
@@ -10,8 +10,10 @@ import AVKit
 
 struct VideoView: View {
     var body: some View {
-        VideoPlayer(player: playUrlVideo(file: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", type: "mp4"))
-    }
+        VStack {
+            VideoPlayer(player: playUrlVideo(file: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", type: "mp4"))
+        }
+        }
 }
 
 struct VideoView_Previews: PreviewProvider {
